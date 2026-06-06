db7b317836c1e97db6db6b63321ef2c2be5ed6df
liyuan <huanglyuan15@126.com>
2026-06-06 18:40:41 +0800

    修改测试波特率2u*1000u*1000u为115200u

--- app/board.c
+++ app/board.c
@@ -3,7 +3,7 @@
 #include "usart_desc.h"
 #include "driver_usart.h"
 
-struct usart_desc usart1_desc = {USART1, 2u*1000u*1000u, USART_StopBits_1};
+struct usart_desc usart1_desc = {USART1, 115200u, USART_StopBits_1};
 usart_desc_t usart_1 = &usart1_desc;
 
 void board_lowlevel_init(void)
